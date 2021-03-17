ef85926c82e38b3fd4d46eed75631bf323e5f916
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-03-17 13:50:36 +0100

    NuGat : Prism.wpf installed

---
+++ TheDebtBook-Assignment1/Models/Dept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace TheDebtBook_Assignment1.Models
+{
+    public class Dept : BindableBase
+    {
+    }
+}
