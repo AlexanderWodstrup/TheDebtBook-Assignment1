f89604b7905dd532bbc09d94ea82545679da553e
pallemanden <58727279+pallemanden@users.noreply.github.com>
2021-03-19 17:42:44 +0100

    3rd window

---
+++ TheDebtBook-Assignment1/ViewModels/DebtHistoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace TheDebtBook_Assignment1.ViewModels
+{
+    public class DebtHistoryViewModel : BindableBase
+    {
+        public DebtHistoryViewModel()
+        {
+        }
+    }
+}
